489a07281e4035ae5a80d4abf7a0e85a981783f0
Rania Charkaoui <48803445+raniacharkaoui@users.noreply.github.com>
2021-05-10 16:18:38 +0200

    Bug fixed on org.maven

---
+++ MISproject2/target/generated-sources/annotations/Model/VaccinationCenter_.java
@@ -0,0 +1,15 @@
+package Model;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2021-05-10T16:15:14", comments="EclipseLink-2.7.7.v20200504-rNA")
+@StaticMetamodel(VaccinationCenter.class)
+public class VaccinationCenter_ { 
+
+    public static volatile SingularAttribute<VaccinationCenter, Integer> availableDoses;
+    public static volatile SingularAttribute<VaccinationCenter, String> municipality;
+    public static volatile SingularAttribute<VaccinationCenter, Integer> id;
+
+}
